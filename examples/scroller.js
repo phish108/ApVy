424fde8c6a2f2fcd5969d30416ae7279c62c40f0
Christian Glahn <cpglahn@gmail.com>
2017-04-30 11:43:17 +0200

    better example without console logs

--- examples/scroller.js
+++ examples/scroller.js
@@ -1,7 +1,19 @@
 class scroller {
+    // catch the scroll event
     scroll(target, evt) {
         // do something on mouse scroll
-        console.log("scroll");
+        let counter = document.getElementById("barScrollCounter"),
+            block   = document.getElementById("barScrollContent");
+
+        counter.textContent = block.scrollTop;
+    }
+
+    // catch the close event
+    close() {
+        // scroll the view back to top, so we can start over
+        document.getElementById("barScrollContent").scrollTop = 0;
+        // reset the counter
+        document.getElementById("barScrollCounter").textContent = 0;
     }
 }
 
